d116594b7cfb848088b19304bcdfd92b597a334b
Azuki19 <analucia27259@gmail.com>
2023-10-28 16:20:13 -0500

    .

---
+++ principal.js
@@ -0,0 +1,43 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href').substr(1); // Obtiene el ID de la sección a la que se va a desplazar.
+        const targetSection = document.getElementById(targetId);
+
+        if (targetSection) {
+            targetSection.scrollIntoView({
+                behavior: 'smooth' // Hace el desplazamiento suave.
+            });
+        }
+    });
+});
+
+function initCarousel() {
+    const items = document.querySelectorAll('.carousel-item');
+    let currentItem = 0;
+
+    function showItem(index) {
+        items.forEach((item, i) => {
+            item.style.opacity = i === index ? 1 : 0;
+        });
+    }
+
+    showItem(currentItem);
+
+    const prevButton = document.querySelector('.arrow-prev');
+    const nextButton = document.querySelector('.arrow-next');
+
+    prevButton.addEventListener('click', () => {
+        currentItem = (currentItem - 1 + items.length) % items.length;
+        showItem(currentItem);
+    });
+
+    nextButton.addEventListener('click', () => {
+        currentItem = (currentItem + 1) % items.length;
+        showItem(currentItem);
+    });
+}
+
+// Llama a la función initCarousel para iniciar el carrusel.
+initCarousel();

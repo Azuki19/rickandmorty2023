ea42004881dac4c4eb78de56f19bfc80e4f3f78c
Azuki19 <analucia27259@gmail.com>
2023-10-28 10:37:00 -0500

    sesion y registro

--- registro.js
+++ registro.js
@@ -1,19 +1,26 @@
 document.addEventListener("DOMContentLoaded", function() {
     const loginform = document.getElementById("login-form");
-    const usernameInput = document.getElementById("user-name");  
+    const usernameInput = document.getElementById("user-name");
+    const emailImput = document.getElementById("email")
     const passwordInput = document.getElementById("password");
     const mensaje = document.getElementById("mensaje");
     const LoginButton = document.getElementById("Login");
   
     LoginButton.addEventListener("click", function() {
-      const username = usernameInput.value.trim();    
+      const username = usernameInput.value.trim();
+      const email = emailImput.value.trim();
       const  password = passwordInput.value;
   
-      if (username === "" || password === "") {
+      if (username === "" || email === "" || password === "") {
         mensaje.textContent = "Por favor, complete todos los campos.";
         return;
     } 
   
+    if (!/^\S+@\S+\.\S+$/.test(email)) {
+      mensaje.textContent = "La dirección de correo no es válida.";
+      return;
+  }
+
     if (passwordInput.value.length < 8) {
       mensaje.textContent = "La contraseña debe tener al menos 8 caracteres.";
       return;

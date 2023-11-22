9fbe7f5d7817b3c4892f626c8786bc727ff6ccfe
Azuki19 <analucia27259@gmail.com>
2023-11-22 02:41:22 -0500

    .

--- Personaje.js
+++ Personaje.js
@@ -1,3 +1,24 @@
+// Función para verificar si un personaje es favorito
+function isFavorite(id) {
+    var activeUser = sessionStorage.getItem('activeUser');
+    let favorites = JSON.parse(localStorage.getItem(`favorites_${activeUser}`)) || [];
+    return favorites.some(fav => fav.id === id);
+}
+
+// Función para agregar un personaje como favorito
+function addFavorite(activeUser, id, name, image) {
+    let favorites = JSON.parse(localStorage.getItem(`favorites_${activeUser}`)) || [];
+    favorites.push({ id, name, image });
+    localStorage.setItem(`favorites_${activeUser}`, JSON.stringify(favorites));
+}
+
+// Función para quitar un personaje de la lista de favoritos
+function removeFavorite(activeUser, id) {
+    let favorites = JSON.parse(localStorage.getItem(`favorites_${activeUser}`)) || [];
+    let updatedFavorites = favorites.filter(fav => fav.id !== id);
+    localStorage.setItem(`favorites_${activeUser}`, JSON.stringify(updatedFavorites));
+}
+
 const urlParams = new URLSearchParams(window.location.search);
 const characterId = urlParams.get('id');
 
@@ -27,32 +48,39 @@ async function fetchCharacterInfo() {
         const imagenPersonaje = document.getElementById('imagen-personaje');
         imagenPersonaje.src = character.image;
         imagenPersonaje.alt = character.name;
+
+        // Obtener referencia al elemento de la estrella
+        const starIcon = document.getElementById('star-icon');
+
+        // Verificar si el personaje ya es favorito y actualizar la estrella en consecuencia
+        if (isFavorite(characterId)) {
+            starIcon.src = 'IMG/baseline_star_white_48dp.png'; // Cambia la imagen a la estrella rellena
+        } else {
+            starIcon.src = 'IMG/baseline_star_outline_white_48dp.png'; // Cambia la imagen a la estrella outline
+        }
+
+        // Agregar evento de clic a la estrella para agregar o quitar el personaje de favoritos
+        starIcon.addEventListener('click', function () {
+            if (isFavorite(characterId)) {
+                // Si el personaje ya es favorito, quitarlo de la lista de favoritos
+                removeFavorite(sessionStorage.getItem('activeUser'), characterId);
+                starIcon.src = 'IMG/baseline_star_outline_white_48dp.png'; // Cambia la imagen a la estrella outline
+            } else {
+                // Si el personaje no es favorito, agregarlo a la lista de favoritos
+                const character = {
+                    id: characterId,
+                    name: nombrePersonaje.textContent,
+                    image: imagenPersonaje.src
+                };
+                addFavorite(sessionStorage.getItem('activeUser'), characterId, character.name, character.image);
+                starIcon.src = 'IMG/baseline_star_white_48dp.png'; // Cambia la imagen a la estrella rellena
+            }
+            // Recargar la página después de agregar/quitar de favoritos para reflejar los cambios en la lista de favoritos
+            location.reload();
+        });
     } catch (error) {
         console.error("Error fetching character data:", error);
     }
 }
 
 fetchCharacterInfo();
-
-// Obtén una referencia al elemento de la estrella
-const starIcon = document.getElementById('star-icon');
-
-// Agrega un evento de clic a la estrella
-starIcon.addEventListener('click', function () {
-    // Obtén la ruta de la imagen actual de la estrella
-    const currentImage = starIcon.src;
-
-    // Define las rutas de las imágenes de la estrella llena y la estrella outline
-    const starOutlineImage = 'IMG/baseline_star_outline_white_48dp.png';
-    const starFilledImage = 'IMG/baseline_star_white_48dp.png'; // Reemplaza 'IMG/star_filled.png' con la ruta de tu imagen de estrella rellena
-
-    // Verifica si la imagen actual es la de la estrella outline
-    if (currentImage.endsWith(starOutlineImage)) {
-        // Cambia la imagen a la estrella rellena
-        starIcon.src = starFilledImage;
-    } else {
-        // Si no es la estrella outline, cambia la imagen a la estrella outline
-        starIcon.src = starOutlineImage;
-    }
-});
-

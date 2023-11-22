2a747e0488eaef27c6d0e3acdaceac8b27a81155
Azuki19 <analucia27259@gmail.com>
2023-11-22 01:21:43 -0500

    .

--- Favorito.js
+++ Favorito.js
@@ -1,6 +1,5 @@
-const favoritesContainer = document.getElementById("favoritos-container");
-
 document.addEventListener("DOMContentLoaded", function() {
+    var favoritesContainer = document.getElementById("favoritos-container");
     var activeUser = sessionStorage.getItem('activeUser');
 
     if (activeUser) {
@@ -11,23 +10,6 @@ document.addEventListener("DOMContentLoaded", function() {
             favoritesContainer.appendChild(tarjeta);
         });
     }
-
-    var inputBusqueda = document.getElementById("barra-busqueda-input");
-
-    inputBusqueda.addEventListener("input", function() {
-        var valorBusqueda = inputBusqueda.value.toLowerCase();
-        var tarjetas = document.getElementsByClassName("Personaje");
-
-        for (var i = 0; i < tarjetas.length; i++) {
-            var tarjeta = tarjetas[i];
-            var nombrePersonaje = tarjeta.querySelector(".name h2").textContent.toLowerCase();
-            if (nombrePersonaje.includes(valorBusqueda)) {
-                tarjeta.style.display = "block";
-            } else {
-                tarjeta.style.display = "none";
-            }
-        }
-    });
 });
 
 function createCharacterCard(character) {
@@ -52,9 +34,13 @@ function createCharacterCard(character) {
     estrella.addEventListener('click', function (event) {
         event.stopPropagation();
         var characterId = estrella.getAttribute('data-id');
+        var activeUser = sessionStorage.getItem('activeUser');
 
         removeFavorite(activeUser, characterId);
         tarjeta.remove();
+
+        // Muestra en la página principal con estrella vacía
+        showInMainWithEmptyStar(character);
     });
 
     return tarjeta;
@@ -64,4 +50,43 @@ function removeFavorite(user, id) {
     let favorites = JSON.parse(localStorage.getItem(`favorites_${user}`)) || [];
     favorites = favorites.filter(fav => fav.id !== id);
     localStorage.setItem(`favorites_${user}`, JSON.stringify(favorites));
+}
+
+function showInMainWithEmptyStar(character) {
+    const container = document.getElementById("personajes-container");
+    const tarjeta = createCharacterCardForMain(character);
+    container.appendChild(tarjeta);
+}
+
+function createCharacterCardForMain(character) {
+    const tarjeta = document.createElement("div");
+    tarjeta.className = "Personaje";
+
+    tarjeta.innerHTML = `
+        <div class="contenedor-verdeo">
+            <div class="contenedor-verdec">
+                <a href="./Personaje.html?id=${character.id}">
+                    <img src="${character.image}" alt="${character.name}">
+                </a>
+            </div>
+            <div class="nameandfav">
+                <div class="name"><h2>${character.name}</h2></div>
+                <div class="estrella estrella-vacia" data-id="${character.id}"></div>
+            </div>
+        </div>
+    `;
+
+    var estrella = tarjeta.querySelector('.estrella');
+    estrella.addEventListener('click', function (event) {
+        event.stopPropagation();
+        var characterId = estrella.getAttribute('data-id');
+        var activeUser = sessionStorage.getItem('activeUser');
+
+        // Añade a favoritos en la página principal
+        addFavorite(activeUser, characterId, character.name, character.image);
+
+        tarjeta.remove();
+    });
+
+    return tarjeta;
 }
